13d198e27ab7ce92a4b3f483d4399d8ac7af56c1
YOUSSEF EL MOURABIT <41373170+By-oussef@users.noreply.github.com>
2020-03-16 22:38:59 +0100

    Update Gallery.php
    Add google analitycs

--- npgmobile/gallery.php
+++ npgmobile/gallery.php
@@ -11,6 +11,16 @@ if (!defined('WEBPATH'))
 
 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109862535-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-109862535-3');
+</script>
+
 
 		<?php
 		scriptLoader($_themeroot . '/style.css');
